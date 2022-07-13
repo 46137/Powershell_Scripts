d06bcd6ffa18899b2b644d6e942595acbb260040
Heady <ljhead.13@gmail.com>
2022-07-13 15:41:16 +1000

    updated

--- Windows_Script_Full-Survey.ps1
+++ Windows_Script_Full-Survey.ps1
@@ -12,12 +12,18 @@ function Get-FullSurvey(){
     function Get-OSInfo(){
         $datetime = [System.DateTime]::now
         $timezone = [System.TimeZoneInfo]::Local
+        $osinfo = Get-WmiObject -Class Win32_OperatingSystem |Select-Object -Property Caption, Version, CSName, OSArchitecture, WindowsDirectory
+        $apps32 = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object InstallDate, DisplayName, DisplayVersion, Publisher, InstallSource |Sort-Object InstallDate -Descending |Format-Table -Wrap
+        $apps64 = Get-ItemProperty HKLM:\software\microsoft\windows\currentversion\Uninstall\* |Select-Object InstallDate, DisplayName, DisplayVersion, Publisher, InstallSource |Sort-Object InstallDate -Descending |Format-Table -Wrap
 
         # Create an empty hashtable.
         $return_data = @{}
         # Add the collected data to the hashtable.
-        $return_data.add("DateTime", $datetime)
-        $return_data.add("TimeZone", $timezone)
+        $return_data.Add("DateTime", $datetime)
+        $return_data.Add("TimeZone", $timezone)
+        $return_data.Add("OSInfo", $osinfo)
+        $return_data.Add("InstalledApps32", $apps32)
+        $return_data.Add("InstalledApps64", $apps64)
 
         # Return the hashtable.
         return $return_data
