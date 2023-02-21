84226f1f26b74c60c82fa96af9080e9fcd58d46a
Heady <ljhead.13@gmail.com>
2023-02-20 21:03:02 -0800

    changes

--- Modules/Auto_Invoke-Modules.ps1
+++ Modules/Auto_Invoke-Modules.ps1
@@ -1,20 +1,23 @@
 #Script intent: When you want to investigate a potentially compromised device, this can be used to invoke specific commands and save into a local running log.
 
-$comp = '172.16.10.101' #'WIN10-TEST'
 #read-host -assecurestring | convertfrom-securestring | out-file C:\secure.txt <- Run this command once to generate your secure password file. 
 $User = "TL\Heady" #"546CMT\Administrator"
 $PWord = Get-Content 'C:\Users\Heady\Desktop\secure.txt' | ConvertTo-SecureString
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
 $DTG = Get-Date -Format "yyMMdd"
 
-$Modules = Get-Content C:\Users\Heady\Documents\Powershell_Scripts-1\Payload_Endpoint-Modules.txt
-foreach ($Module in $Modules){
-    $output = Invoke-Command -FilePath $Module -ComputerName $comp -Credential $Credential
+#Select the hosts text file. E.g. 230220-Hosts-Port-5985
+$Hosts = Get-Content 'C:\Users\Heady\Desktop\230220-Hosts-Port-5985.txt'
+foreach ($H in $Hosts){
 
-    $output
+    $Modules = Get-Content C:\Users\Heady\Documents\Powershell_Scripts-1\Modules\Payload_Endpoint-Modules.txt
+    foreach ($Module in $Modules){
+        $output = Invoke-Command -FilePath $Module -ComputerName $H -Credential $Credential
 
-    #There are two output methods, to a appending text log or json. Comment out the one not needed.
-    #$output | Out-File -Append C:\Users\heady\Desktop\$DTG-$comp-log.txt
-    $output | ConvertTo-Json | Out-File -Append C:\Users\heady\Desktop\$DTG-$comp-data.json
+        $output
 
+        #There are two output methods, to a appending text log or json. Comment out the one not needed.
+        #$output | Out-File -Append C:\Users\heady\Desktop\$DTG-$H-log.txt
+        $output | ConvertTo-Json | Out-File -Append C:\Users\heady\Desktop\$DTG-$H-data.json
+    }
 }
