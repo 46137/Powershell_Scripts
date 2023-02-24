98469664568125badf6efc557e60a0e358d930c4
Heady <ljhead.13@gmail.com>
2023-02-23 18:02:14 -0800

    changes

---
+++ Modules/BT-Netstat (Incomplete).ps1
@@ -0,0 +1,39 @@
+function BT-Netstat(){
+    <#
+    .DESCRIPTION
+    To Run: Execute this file. (OR)
+    To Run: Import-Module .\BT-Netstat.ps1 -Force 
+
+     UDP equivilent.
+    executablepath or just path from get-process
+
+    #>
+    [CmdletBinding()]
+    param()
+    $ErrorActionPreference = 'SilentlyContinue' # Disables errors.
+    #Raw data.
+    $raw = Get-NetTCPConnection
+    
+    # Creating an array to store filtered information.
+    $Filtered = @()
+    $raw |ForEach-Object {
+        $Return_Data = New-Object -TypeName PSObject |Select-Object -Property CreationTime, PID, ProcessName, State, SrcAddress, SrcPort, DstAddress, DstPort, NetworkProfile, ExecutablePath 
+        $Return_Data.CreationTime = $_.CreationTime
+        $Return_Data.PID = $_.OwningProcess
+        $Return_Data.ProcessName = (Get-Process -Id $_.OwningProcess).ProcessName
+        $Return_Data.State = $_.State
+        $Return_Data.SrcAddress = $_.LocalAddress
+        $Return_Data.SrcPort = $_.LocalPort
+        $Return_Data.DstAddress = $_.RemoteAddress
+        $Return_Data.DstPort = $_.RemotePort
+        #$Return_Data.ExecutablePath = (Get-Process -Id $_.OwningProcess).ExecutablePath
+       
+        $Return_Data.NetworkProfile = $_.AppliedSetting
+
+
+        $Filtered += $Return_Data
+    }
+    $Filtered
+}
+#Calling the function below will output the results when run.
+BT-Netstat
